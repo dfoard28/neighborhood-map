a421be090e78916764a2792a127209063801af84
Desiree Foard <foard.desiree@gmail.com>
2018-01-01 13:02:43 -0700

    updated index.html with bindings for filter function. Added in a renderLocations function and a filter function on the app.js file

--- js/app.js
+++ js/app.js
@@ -4,59 +4,131 @@ var markers = [
     {
         position: {lat: 33.515349, lng: -117.755575},
         title: 'La Serina Grill',
-        type: 'Food',
-        show: true
+        type: 'Food'
 
     },
     {
         position: {lat: 33.514616, lng: -117.759609},
         title: 'Treasure Island Beach',
-        type: 'Beach',
-        show: true
+        type: 'Beach'
     },
     {
         position: {lat: 33.515848, lng: -117.756028},
         title: 'Star-fish Laguna Beach',
-        type: 'Food',
-        show: true
+        type: 'Food'
     },
     {
         position: {lat: 33.514953, lng: -117.757155},
         title: 'Montage Laguna Beach',
-        type: 'Hotel',
-        show: true
+        type: 'Hotel'
     },
     {
         position: {lat: 33.524058, lng: -117.764995},
         title: 'Casa Laguna Hotel and Spa',
-        type: 'Hotel',
-        show: true
+        type: 'Hotel'
     },
     {
         position: {lat: 33.520655, lng: -117.764603},
         title: 'Pirate Tower',
-        type: 'Beach',
-        show: true
+        type: 'Beach'
     },
     {
         position: {lat: 33.513560, lng: -117.755424},
         title: 'Treasure Island Park',
-        type: 'Park',
-        show: true
+        type: 'Park'
     }
 ];
-//view model function
-//TODO: add marker creation here?
-var ViewModel = function () {
-    //TODO: infowindow should open and marker should bounce when list item is clicked.
-    this.location = ko.observableArray(markers);
-    this.position = location.position;
-    this.title = location.title;
-    this.type = location.type;
 
+var Location = function (data) {
+    this.postion = data.position;
+    this.title = data.title;
+    this.type = data.type;
 };
 
-ko.applyBindings(new ViewModel());
+//viewmodel function
+var ViewModel = function () {
+    var self = this;
+    self.types = ['Food', 'Beach', 'Hotel', 'Park', 'All'];
+    self.locations = ko.observableArray([]);
+    self.markers = [];
+    var renderLocations = function (filteredMarkers) {
+        ko.utils.arrayForEach(self.locations(), function (location) {
+            console.log(location.marker);
+            location.marker.setMap(null);
+        })
+        self.locations.removeAll();
+        filteredMarkers.forEach(function (loca) {
+            var location = new Location(loca);
+          //  self.locations.push(location);
+            location.marker = new google.maps.Marker({
+                position: loca.position,
+                map: map,
+                animation: google.maps.Animation.DROP,
+                title: loca.title
+            });
+            location.marker.addListener('click', function () {
+                self.activateMarker(this);
+            });
+            self.activateMarker = function (marker) {
+                var self = marker.marker !== undefined ? marker.marker : marker;
+                var info = new google.maps.InfoWindow({
+                    content: self.title
+                });
+                info.open(map, self);
+                if (self.getAnimation() !== null) {
+                    self.setAnimation(null);
+                } else {
+                    self.setAnimation(google.maps.Animation.BOUNCE);
+                    setTimeout(function () {
+                        self.setAnimation(null);
+                    }, 2000);
+                }
+            }
+            self.locations.push(location);
+        });
+    }
+ /*   markers.forEach(function (loca) {
+        var location = new Location(loca);
+      //  self.locations.push(location);
+        location.marker = new google.maps.Marker({
+            position: loca.position,
+            map: map,
+            animation: google.maps.Animation.DROP,
+            title: loca.title
+        });
+        location.marker.addListener('click', function () {
+            self.activateMarker(this);
+        });
+        self.activateMarker = function (marker) {
+            var self = marker.marker !== undefined ? marker.marker : marker;
+            var info = new google.maps.InfoWindow({
+                content: self.title
+            });
+            info.open(map, self);
+            if (self.getAnimation() !== null) {
+                self.setAnimation(null);
+            } else {
+                self.setAnimation(google.maps.Animation.BOUNCE);
+                setTimeout(function () {
+                    self.setAnimation(null);
+                }, 2000);
+            }
+        }
+        self.locations.push(location);
+    });*/
+            renderLocations(markers);
+    self.filter = function (criteria) {
+        if(criteria === 'All'){
+            renderLocations(markers);
+        } else {
+        var locations = markers.filter(function (loc) {
+                return loc.type === criteria;
+        });
+            renderLocations(locations);
+        }
+    };
+};
+
 
 //map variable
 var map;
@@ -146,10 +218,16 @@ function initMap() {
                 stylers: [{color: '#17263c'}]
             }
         ]
-    });
 
-    //forEach loop that adds a marker, and info window and an event listener for the info window to the map
-    markers.forEach(function (element) {
+    });
+    var viewModel = new ViewModel();
+    ko.applyBindings(viewModel);
+   // addMar(viewModel);
+}
+
+//forEach loop that adds a marker, info window, and an event listener for the info window to the map
+/*function addMar(viewModel) {
+    ko.utils.arrayForEach(viewModel.locations, function (element) {
         var mark = new google.maps.Marker({
             position: element.position,
             map: map,
@@ -163,23 +241,25 @@ function initMap() {
             info.open(map, mark);
             toggleBounce();
         });
+
     //function to make markers bounce when clicked on (attribute to google maps api)
         function toggleBounce() {
             if (mark.getAnimation() !== null) {
-              mark.setAnimation(null);
+                mark.setAnimation(null);
             } else {
-              mark.setAnimation(google.maps.Animation.BOUNCE);
+                mark.setAnimation(google.maps.Animation.BOUNCE);
               setTimeout(function () {mark.setAnimation(null);}, 2000);
             }
         }
     });
-}
+}*/
+
 //function to handle errors
 function errors() {
     alert("failed to load")
 }
 
-//TODO: add an is-active class to the dropdown element when clicked (do in Knockout?)
+//add an is-active class to the dropdown element when clicked, this is required by bulma for dropdown
 $('.dropdown').click(function () {
     $(this).toggleClass('is-active');
 });
